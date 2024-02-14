3df4b0277857369c29a338cc8b4db52d3a6ddf8c
rouzwelt <rouzweltt@gmail.com>
2024-02-14 16:06:07 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -45,6 +45,32 @@
           rainix.node-build-inputs.${system}
         ] ++ (with pkgs; [ 
           wasm-bindgen-cli
+          libgobject-2
+          libglib-2
+          libnss3
+          libnssutil3
+          libsmime3
+          libnspr4
+          libatk-1
+          libatk-bridge-2
+          libcups
+          libgio-2
+          libdrm
+          libdbus-1
+          libexpat
+          libxcb
+          libxkbcommon
+          libatspi
+          libX11
+          libXcomposite
+          libXdamage
+          libXext
+          libXfixes
+          libXrandr
+          libgbm
+          libpango-1
+          libcairo
+          libasound
         ]);
       };
     }
