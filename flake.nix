0bc85d66b03c4479499c251ca758773e28dd418f
rouzwelt <rouzweltt@gmail.com>
2024-08-06 01:51:11 +0000

    update

--- flake.nix
+++ flake.nix
@@ -10,31 +10,11 @@
     let
       pkgs = rainix.pkgs.${system};
     in rec {
-      packages = {
-        install = rainix.mkTask.${system} {
-          name = "install";
-          body = ''
-            set -euxo pipefail
-            npm install
-          '';
-          additionalBuildInputs = [
-            pkgs.wasm-bindgen-cli
-            rainix.rust-toolchain.${system}
-            rainix.rust-build-inputs.${system}
-            rainix.node-build-inputs.${system}
-          ];
-        };
-      };
-
       # For `nix develop`:
       devShell = pkgs.mkShell {
         nativeBuildInputs = [
-          rainix.rust-toolchain.${system}
-          rainix.rust-build-inputs.${system}
           rainix.node-build-inputs.${system}
-        ] ++ (with pkgs; [ 
-          wasm-bindgen-cli
-        ]);
+        ];
       };
     }
   );
